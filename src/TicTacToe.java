c7d5a9661995c5d1033eaf1b739c8d3d35b9086e
BuildTools <jmeredit@ashland.edu>
2018-02-11 21:04:47 -0500

    Fixed number verification

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,7 +7,7 @@ public class TicTacToe {
 		Scanner scan = new Scanner(System.in);
 		
 		//Variables
-		String[] board = new String[10];
+		String[] board = new String[9];
 		int win = 0;		
 		int change;
 
@@ -23,23 +23,29 @@ public class TicTacToe {
 		board[8] = "9";
 		
 		//Allow User to choose game piece
-		String gamePiece;		
+		String gamePiece = "error";		
 		do {
 			System.out.println("Choose your game piece (X or O):");
 			gamePiece = scan.nextLine();
-		} while (gamePiece != "X" || gamePiece != "O");
+		} while (gamePiece == "X" || gamePiece == "O");
 		
 		do {
 			display(board);
 			do {
-				System.out.print("Choose a number: ");
-				change = scan.nextInt();
-			} while (board[change] == "X" || board[change] == "O");
-		change = change - 1;
+				do{
+					System.out.print("Choose a number: ");
+					change = scan.nextInt();
+					change = (change - 1);
+				} while (change >=8 && change <=0);
+			} while ((board[change] == "X" || board[change] == "O"));
 		board[change] = gamePiece;
+		win = check(board);
 		} while (win == 0);
+		System.out.println("Game over");
+		System.exit(0);
 	}
 	
+	//Check for a win
 	private static int check(String board[]){
 		int value = 0;
 		if (board[0]== board[1] && board[1] == board[2]) {
@@ -52,11 +58,14 @@ public class TicTacToe {
 			value = 1;
 		} else if (board[8] == board[4] && board[4] == board[0]) {
 			value = 1;
+		} else {
+			value = 0;
 		}
 		
 		return value;
 	}
 
+	//Display the board
 	private static void display(String board[]) {
 		System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
 		System.out.println("---------");
